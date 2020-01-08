891e12a2c3eb465026d4e119bd98ef3e2af6d941
georgetegral <georgetegral@gmail.com>
2020-01-07 22:27:43 -0600

    Send message finished

--- src/app/containers/default-layout/default-layout.component.ts
+++ src/app/containers/default-layout/default-layout.component.ts
@@ -1,5 +1,7 @@
 import {Component } from '@angular/core';
 import { navItems } from '../../_nav';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,11 +12,38 @@ export class DefaultLayoutComponent {
   public navItems = navItems;
   year: number = new Date().getFullYear();
 
-  form:any = {};
+  nameF: FormControl;
+  mailF: FormControl;
+  subjF: FormControl;
+  messF: FormControl;
+
+  constructor(private afDB: AngularFireDatabase){
+    this.nameF = new FormControl('',[Validators.required]);
+    /*
+    this.nameF.valueChanges
+      .subscribe(value => {
+        console.log(value);
+      });
+    */
+    this.mailF = new FormControl('',[
+      Validators.required,
+      Validators.pattern(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/)
+    ]);
+    this.subjF = new FormControl('',[Validators.required]);
+    this.messF = new FormControl('',[Validators.required]);
+  }
 
   sendForm(){
+    var id = Date.now();
+    this.afDB.database.ref('messages/'+id+'/name').set(this.nameF.value);
+    this.afDB.database.ref('messages/'+id+'/email').set(this.mailF.value);
+    this.afDB.database.ref('messages/'+id+'/subject').set(this.subjF.value);
+    this.afDB.database.ref('messages/'+id+'/message').set(this.messF.value);
     alert('Message sent correctly');
-    this.form = {}
+    this.nameF.setValue("");  this.nameF.reset();
+    this.mailF.setValue("");  this.mailF.reset();
+    this.subjF.setValue("");  this.subjF.reset();
+    this.messF.setValue("");  this.messF.reset();
   }
 
   goToLink(url: string){
