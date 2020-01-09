30e8142ed20424d70b8ece8585657df11079b047
georgetegral <georgetegral@gmail.com>
2020-01-08 19:38:48 -0600

    Preparing to upload to github

--- src/app/_nav.ts
+++ src/app/_nav.ts
@@ -2,13 +2,9 @@ import { INavData } from '@coreui/angular';
 
 export const navItems: INavData[] = [
   {
-    name: 'Dashboard',
-    url: '/dashboard',
-    icon: 'icon-speedometer',
-    badge: {
-      variant: 'info',
-      text: 'NEW'
-    }
+    name: 'Home',
+    url: '/home',
+    icon: 'icon-home'
   },
   {
     title: true,
