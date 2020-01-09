c0d642ac14efdac0a84444084236d4cdbb6401db
georgetegral <georgetegral@gmail.com>
2020-01-08 23:35:37 -0600

    braintegral module done

--- src/app/views/braintegral/braintegral.module.ts
+++ src/app/views/braintegral/braintegral.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BraintegralComponent } from './braintegral.component';
 import { BraintegralRoutingModule } from './braintegral-routing.module';
-// import ngx-translate and the http loader
-import {TranslateModule} from '@ngx-translate/core';
+
+// My imports
+import { TranslateModule } from '@ngx-translate/core';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   imports: [
     BraintegralRoutingModule,
-    TranslateModule
+    TranslateModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule
   ],
   declarations: [ BraintegralComponent ],
   exports: []
 })
-export class BraintegralModule { }
+export class BraintegralModule { 
+  constructor(){ }
+}
