95804888ec094c188df8bd343797c2fe1c6d249e
georgetegral <georgetegral@gmail.com>
2020-01-13 19:19:24 -0600

    Home finished and Navbar looking good

--- src/app/views/home/home.module.ts
+++ src/app/views/home/home.module.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
 // import ngx-translate and the http loader
 import {TranslateModule} from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   imports: [
     HomeRoutingModule,
-    TranslateModule
+    TranslateModule,
+    CommonModule
   ],
   declarations: [ HomeComponent ],
   exports: []
